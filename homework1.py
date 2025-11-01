91e087ada021f8e22eef0fecf986c9e76a933bd6
Sample327 <surma.toko@gmail.com>
2025-11-01 17:18:58 +0400

    added personal info

--- homework1.py
+++ homework1.py
@@ -1,2 +1,17 @@
 name = "Tornike"
 
+age = 25
+
+have_coded_before = True
+
+print("Name", name)
+print("age", age)
+
+if have_coded_before:
+    print("yes,i have coded before")
+    
+else:
+    print("no i didnot")
+
+
+
